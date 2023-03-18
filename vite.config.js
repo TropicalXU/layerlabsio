32e7d24f801e43ec0e010ed0947e178db47f23f1
Sean Hackett <seanhackett@Seans-MacBook-Pro.local>
2023-03-17 23:17:58 -0400

    new updates

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,12 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    REACT_APP_SERVICE_ID: process.env.VITE_SERVICE_ID,
-    REACT_APP_VITE_TEMPLATE_ID: process.env.VITE_TEMPLATE_ID,
-    REACT_APP_VITE_USER_ID: process.env.VITE_USER_ID
-  }
+  // define: {
+  //   build: {
+  //     outDir: 'dist',
+  //     assetsDir: 'assets',
+  //     minify: true,
+  //     sourcemap: false,
+  //   },
+  // }
 })
